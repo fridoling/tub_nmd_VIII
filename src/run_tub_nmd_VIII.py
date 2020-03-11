9ffffecce88e5d5480e856fd79a6c4dd825cb5b1
Fridolin Gross <fridoling@hotmail.com>
2020-03-11 18:34:28 +0100

    save to proper location

--- src/run_tub_nmd_VIII.py
+++ src/run_tub_nmd_VIII.py
@@ -6,7 +6,6 @@ import shutil
 import pickle
 from datetime import datetime
 import multiprocessing as mp
-import pandas as pd
 
 
 from tub_nmd_VIII_model import * 
@@ -21,24 +20,16 @@ os.mkdir(res_folder)
 fitness_cdc20x1 = np.zeros((2,2,2))
 fitness_cdc20x4 = np.zeros((2,2,2))
 
-df_fitness = pd.read_csv('../tub_nmd_VIII/src/fitness.csv', dtype={'genotype': str})
-df_fitness.set_index('genotype', inplace=True)
-
-for ind, f1, f4 in df_fitness.itertuples():
-    fitness_cdc20x1[int(ind[0]), int(ind[1]), int(ind[2])] = f1    
-    fitness_cdc20x4[int(ind[0]), int(ind[1]), int(ind[2])] = f4
-
-for ind, f1, f4 in df_fitness.itertuples():
-    fitness_cdc20x1[int(ind[0]), int(ind[1]), int(ind[2])] = f1    
-    fitness_cdc20x4[int(ind[0]), int(ind[1]), int(ind[2])] = f4
+with open('src/fitness.pickle', 'rb') as f:
+    fitness = pickle.load(f)
 
 # define mutation rates
 mut_prob_cdc20x1 = np.array([[1e-6, 1e-7, 0.001], [1e-7, 1e-5, 0.0001]])*1000
 mut_prob_cdc20x4 = np.array([[1e-6, 1e-7, 0.001], [1e-7, 1e-5, 0.0001]])*1000
 
 # define and run experiments
-exp_cdc20x1 = Experiment(fitness_cdc20x1, mut_prob_cdc20x1, size=100)
-exp_cdc20x4 = Experiment(fitness_cdc20x4, mut_prob_cdc20x4, size=100)
+exp_cdc20x1 = Experiment(fitness['cdc20x1'], mut_prob_cdc20x1, size=100)
+exp_cdc20x4 = Experiment(fitness['cdc20x4'], mut_prob_cdc20x4, size=100)
 
 exp_cdc20x1.run(generations=200)
 exp_cdc20x4.run(generations=200)
@@ -51,5 +42,5 @@ for i,exp,title in zip(range(2), [exp_cdc20x1, exp_cdc20x4], ['Cdc20x1', 'Cdc20x
     res[title]['gene_freqs'] = exp.get_gene_freqs()
     res[title]['mean_fitness'] = exp.get_mean_fitness()
 
-with open('res/simulation_data.pickle', 'wb') as f:
+with open(res_folder+'simulation_data.pickle', 'wb') as f:
     pickle.dump(res, f)
