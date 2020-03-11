b969ce52e1719ef917df6835dede092f32ecd77a
Fridolin Gross <fridoling@hotmail.com>
2020-03-11 16:43:01 +0100

    df_fitness instead of fitness

--- src/run_tub_nmd_VIII.py
+++ src/run_tub_nmd_VIII.py
@@ -17,7 +17,7 @@ res_folder = "./res/"+str(datetime.now()).split('.')[0]+'/'
 # read fitness data
 fitness_cdc20x1 = np.zeros((2,2,2))
 fitness_cdc20x4 = np.zeros((2,2,2))
-fitness = pd.read_csv('src/fitness.csv')
+df_fitness = pd.read_csv('src/fitness.csv')
 for ind, f1, f4 in df_fitness.itertuples():
     fitness_cdc20x1[int(ind[0]), int(ind[1]), int(ind[2])] = f1    
     fitness_cdc20x4[int(ind[0]), int(ind[1]), int(ind[2])] = f4
