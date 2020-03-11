cf6a0564cbae32f9d7319dca56541e70597669e8
Fridolin Gross <fridoling@hotmail.com>
2020-03-11 16:29:29 +0100

    complete run file

--- src/run_tub_nmd_VIII.py
+++ src/run_tub_nmd_VIII.py
@@ -12,3 +12,36 @@ from tub_nmd_VIII_model import *
 
 
 res_folder = "./res/"+str(datetime.now()).split('.')[0]+'/'
+
+# read fitness data
+fitness_cdc20x1 = np.zeros((2,2,2))
+fitness_cdc20x4 = np.zeros((2,2,2))
+fitness = pd.read_csv('../tub_nmd_VIII/src/fitness.csv')
+for ind, f1, f4 in df_fitness.itertuples():
+    fitness_cdc20x1[int(ind[0]), int(ind[1]), int(ind[2])] = f1    
+    fitness_cdc20x4[int(ind[0]), int(ind[1]), int(ind[2])] = f4
+
+# define mutation rates
+mut_prob_cdc20x1 = np.array([[1e-6, 1e-7, 0.001], [1e-7, 1e-5, 0.0001]])*1000
+mut_prob_cdc20x4 = np.array([[1e-6, 1e-7, 0.001], [1e-7, 1e-5, 0.0001]])*1000
+
+# define experiments
+exp_cdc20x1.run(generations=200)
+exp_cdc20x4.run(generations=200)
+
+# create plots
+fig,ax = plt.subplots(3,2, figsize=(8,7),sharex=True, sharey='row')
+for i,exp,title in zip(range(2), [exp_cdc20x1, exp_cdc20x4], ['Cdc20x1', 'Cdc20x4']):
+    exp.plot_genotype_freqs(ax=ax[0,i])
+    ax[0,i].set_xlabel(None)
+    ax[0,i].set_title(title, fontweight='bold')
+    exp.plot_gene_freqs(ax=ax[1,i])
+    ax[1,i].set_xlabel(None)
+    exp.plot_mean_fitness(ax=ax[2,i])
+for i in range(3):
+    ax[i,1].set_ylabel(None)
+ax[0,1].legend(bbox_to_anchor=(1,1))    
+ax[1,1].legend(bbox_to_anchor=(1,1))
+plt.tight_layout(h_pad=0, w_pad=0)
+# plt.savefig(res_folder+'simulation.pdf')
+
