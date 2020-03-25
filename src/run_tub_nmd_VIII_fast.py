7f2a4370e95b2080c7049ddacd5f458f688411aa
Fridolin Gross <fridoling@hotmail.com>
2020-03-25 14:47:36 +0100

    implementation of fast model

---
+++ src/run_tub_nmd_VIII_fast.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+import pickle
+import os
+import sys
+from datetime import datetime
+import multiprocessing as mp
+
+from tub_nmd_VIII_fast import * 
+
+if len(sys.argv)>4:
+    res_folder = "./res/"+sys.argv[4]+'/'
+else:
+    res_folder = "./res/"+str(datetime.now()).split('.')[0]+'/'
+if not os.path.isdir('res'):
+    os.mkdir('res')
+if not os.path.isdir(res_folder):
+    os.mkdir(res_folder)
+
+# read fitness data
+with open('src/fitness.pickle', 'rb') as f:
+    fitness = pickle.load(f)
+
+fitness['cdc20x1'][1,0,] = 0    
+fitness['cdc20x4'][1,0,] = 0    
+
+# define mutation rates
+mut_prob = {}
+mut_prob['cdc20x1'] = np.array([[1e-8, 0.0, 0.001], [0.0, 1e-7, 0.0001]])
+mut_prob['cdc20x4'] = np.array([[1e-8, 0.0, 0.001], [0.0, 1e-7, 0.0001]])
+
+
+params = {}
+params['mut_prob'] = mut_prob
+params['fitness'] = fitness
+
+
+# define and run experiments
+params['pop_size'] = np.int(sys.argv[1])
+params['generations'] = np.int(sys.argv[2])
+params['repeats'] = np.int(sys.argv[3])
+
+genotype_in = np.zeros(8)
+ancestor = '010'
+ancestor_dec = int(ancestor, 2)
+genotype_in[ancestor_dec] = params['pop_size']
+
+res = {}
+for label in ['cdc20x1', 'cdc20x4']:
+    res[label] = {}
+    gt_array = np.zeros((params['repeats'], params['generations'], 8))
+    gf_array = np.zeros((params['repeats'], params['generations'], 3))
+    mf_array = np.zeros((params['repeats'], params['generations']))
+    sys.stdout.write('run experiment '+label+'\n')
+    for n in range(params['repeats']):
+        gt_array[n,0,:] = genotype_in
+        for i in progressbar(range(1, params['generations']), "Repeat "+str(n+1), 40):    
+            gt_mut = mutate(gt_array[n,i-1,:], convert_mut(mut_prob[label]))
+            gt_prop = propagate(gt_mut, convert_fitness(fitness[label]))
+            gt_array[n,i,:] = gt_prop
+    gt_array = gt_array/gt_array.sum(axis=2)[:,:,None]
+    gf_array = get_gene_freqs3(gt_array)
+    mf_array = get_mean_fitness3(gt_array, convert_fitness(fitness[label]))
+    res[label]['genotype_freqs'] = {}
+    res[label]['genotype_freqs']['all'] = gt_array
+    res[label]['genotype_freqs']['median'] = np.median(gt_array, axis=0)
+    res[label]['genotype_freqs']['upper'] = np.percentile(gt_array, 95, axis=0)
+    res[label]['genotype_freqs']['lower'] = np.percentile(gt_array, 5, axis=0)
+    res[label]['gene_freqs'] = {}
+    res[label]['gene_freqs']['median'] = np.median(gf_array, axis=0)
+    res[label]['gene_freqs']['upper'] = np.percentile(gf_array, 95, axis=0)
+    res[label]['gene_freqs']['lower'] = np.percentile(gf_array, 5, axis=0)
+    res[label]['mean_fitness'] = {}
+    res[label]['mean_fitness']['median'] = np.median(mf_array, axis=0)
+    res[label]['mean_fitness']['upper'] = np.percentile(mf_array, 95, axis=0)
+    res[label]['mean_fitness']['lower'] = np.percentile(mf_array, 5, axis=0)    
+    
+with open(res_folder+'simulation_data.pickle', 'wb') as f:
+    pickle.dump((res, params), f)
